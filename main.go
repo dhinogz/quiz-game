1a69539dd32e79cea55caacb7fcd19b27f6f0d15
dhinogz <davidhinojosagzz@gmail.com>
2023-07-07 02:22:58 -0600

    Added csv file name flag and refactor

--- main.go
+++ main.go
@@ -1,73 +1,62 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 )
 
-type QuizGame struct {
-	Index    int
-	Question string
-	Answer   string
-}
-
-func createQuizGame(data [][]string) []QuizGame {
-	var quizList []QuizGame
-	for i, line := range data {
-		var quiz QuizGame
-		quiz.Index = i + 1
-		for j, field := range line {
-			if j == 0 {
-				quiz.Question = strings.TrimSpace(field)
-			} else if j == 1 {
-				quiz.Answer = strings.TrimSpace(field)
-			}
-		}
-		quizList = append(quizList, quiz)
-	}
-	return quizList
-}
-
 func main() {
-	fmt.Println("Hello quiz game!")
-
 	// TODO: Parse csv_file flag to read CSV file (add a default csv file)
-	fileName := "problems.csv"
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in format 'question,answer'")
+	flag.Parse()
 	// TODO: Parse time_limit flag to limit quiz time (add a default quiz time limit) Goroutines are needed here
 
-	f, err := os.Open(fileName)
+	f, err := os.Open(*csvFilename)
 	if err != nil {
-		log.Fatal(err)
+		exit("Failed to read CSV file.")
 	}
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
-	data, err := csvReader.ReadAll()
+	lines, err := csvReader.ReadAll()
 	if err != nil {
-		log.Fatal(err)
+		exit("Failed to parse CSV file.")
 	}
+	problems := createProblems(lines)
 
-	quizList := createQuizGame(data)
-
-	var score int
-	reader := bufio.NewReader(os.Stdin)
-	for _, q := range quizList {
-		fmt.Printf("Problem #%d: %s = ", q.Index, q.Question)
-		input, err := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if input == "" {
-			fmt.Println("No answer")
-		}
-		if input == q.Answer {
+	score := 0
+	for i, q := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, q.question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == q.answer {
 			score++
 		}
 	}
-	fmt.Println(score)
+	fmt.Printf("You scored %d out of %d\n", score, len(problems))
+}
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func createProblems(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return problems
+}
+
+func exit(msg string) {
+	log.Fatal(msg)
+	os.Exit(1)
 }
